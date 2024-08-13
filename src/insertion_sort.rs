38b546db754636c92acbcb32023dafab4bc1a9ba
carlomanso <carlosmanzasola@gmail.com>
2024-08-13 02:53:46 +0200

    reformateo del codigo y comentarios para proximos pasos

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -1,14 +1,14 @@
 /**
 Este algoritmo utiliza la implementación clasica del insertion sort
-**/
-pub fn insertion_sort (arr: &mut [i32], ind1:usize, ind2:usize){
-    for i in ind1..ind2{
+ **/
+pub fn insertion_sort(arr: &mut [i32], ind1: usize, ind2: usize) {
+    for i in ind1..ind2 {
         let t = arr[i];
-        let mut j=i;
-        while j>0 && arr[j-1]>t{
-            arr[j]=arr[j-1];
-            j=j-1;
+        let mut j = i;
+        while j > 0 && arr[j - 1] > t {
+            arr[j] = arr[j - 1];
+            j = j - 1;
         }
-        arr[j]=t;
+        arr[j] = t;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::time::Instant;
+
+use rand::Rng;
+
 mod insertion_sort;
 mod selection_sort;
 mod mergesort;
@@ -6,56 +10,59 @@ mod quicksort;
 mod shellsort;
 mod gnome_sort;
 
-use std::time::Instant;
-use rand::Rng;
-
 const LEN: usize = 100;
 
 fn main() {
-    let inicio=Instant::now();
-    let arr=generador_array();
-    println!("Tiempo generando array: {:?}",inicio.elapsed());
-    
-    let inicio=Instant::now();
-    let shuffle_arr=shuffle(arr);
-    println!("{:?}\nTiempo shuffle: {:?}\n",shuffle_arr,inicio.elapsed());
-    
-    let mut aux=shuffle_arr.clone();
-    
-    let inicio=Instant::now();
-    selection_sort::selection_sort(&mut aux,0,LEN);
-    println!("Algoritmo de selección(Selection sort)\n  {:?}\nTiempo ordenación: {:?}\n",aux,inicio.elapsed());
-    assert_eq!(aux,arr);
+    let inicio = Instant::now();
+    let arr = generador_array();
+    println!("Tiempo generando array: {:?}", inicio.elapsed());
 
-    aux=shuffle_arr.clone();
 
-    let inicio=Instant::now();
-    insertion_sort::insertion_sort(&mut aux,0,LEN);
-    println!("Algoritmo de inserción(Insertion sort)\n{:?}\nTiempo ordenación: {:?}\n",aux,inicio.elapsed());
-    assert_eq!(aux,arr);
+    let inicio = Instant::now();
+    let shuffle_arr = shuffle(arr);
+    println!("{:?}\nTiempo shuffle: {:?}\n", shuffle_arr, inicio.elapsed());
 
-    aux=shuffle_arr.clone();
+    let mut aux = shuffle_arr.clone();
+    let inicio = Instant::now();
+    selection_sort::selection_sort(&mut aux, 0, LEN);
+    println!("Algoritmo: Selection sort\n  {:?}\nTiempo ordenación: {:?}\n", aux, inicio.elapsed());
+    assert_eq!(aux, arr);
 
-    let inicio=Instant::now();
-    gnome_sort::gnome_sort(&mut aux,0,LEN);
-    println!("Algoritmo de gnome(Gnome sort)\n{:?}\nTiempo ordenación: {:?}\n",aux,inicio.elapsed());
-    assert_eq!(aux,arr);
-    
+    aux = shuffle_arr.clone();
+
+    let inicio = Instant::now();
+    insertion_sort::insertion_sort(&mut aux, 0, LEN);
+    println!("Algoritmo. Insertion sort\n{:?}\nTiempo ordenación: {:?}\n", aux, inicio.elapsed());
+    assert_eq!(aux, arr);
+
+    aux = shuffle_arr.clone();
+
+    let inicio = Instant::now();
+    gnome_sort::gnome_sort(&mut aux, 0, LEN);
+    println!("Algoritmo: Gnome sort\n{:?}\nTiempo ordenación: {:?}\n", aux, inicio.elapsed());
+    assert_eq!(aux, arr);
+
+    aux = shuffle_arr.clone();
+
+    let inicio = Instant::now(); // Causa un overflow del stack, ya que al utilizar el tipo de dato vec!, que es un tipo dinamico(se utiliza para asignar el tamaño de los vectores de forma dinamica), gasta mas recursos del stack.
+    mergesort::mergesort(&mut aux, 0, LEN);
+    println!("Algoritmo: Merge sort\n{:?}\nTiempo ordenación: {:?}\n", aux, inicio.elapsed());
+    assert_eq!(aux, arr);
 }
 
-fn shuffle(mut arr:[i32;LEN]) -> [i32;LEN]{
-    let mut rng =rand::thread_rng();
-    for i in 0..LEN{
+fn shuffle(mut arr: [i32; LEN]) -> [i32; LEN] {
+    let mut rng = rand::thread_rng();
+    for i in 0..LEN {
         let indx: usize = rng.gen_range(0..LEN);
-        arr.swap(indx,i);
+        arr.swap(indx, i);
     }
     return arr;
 }
 
-fn generador_array() -> [i32;LEN]{
-    let mut arr:[i32;LEN]=[0;LEN];
-    for i in 0..LEN{
-        arr[i]=i as i32;
+fn generador_array() -> [i32; LEN] {
+    let mut arr: [i32; LEN] = [0; LEN];
+    for i in 0..LEN {
+        arr[i] = i as i32;
     }
     return arr;
 }
