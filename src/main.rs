d64695ab213dd20f412be098880afce6d78ad7e3
carlomanso <carlosmanzasola@gmail.com>
2024-08-13 18:42:14 +0200

    añadido de archivos de algoritmos de ordenación, intento de arreglo del algoritmo mergesort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::mem;
 use std::time::Instant;
 
 use rand::Rng;
@@ -9,19 +10,58 @@ mod heapsort;
 mod quicksort;
 mod shellsort;
 mod gnome_sort;
+mod counting_sort;
+mod bucket_sort;
+mod radix_sort;
+mod bubble_sort;
+mod comb_sort;
+mod exchange_sort;
+mod timsort;
+mod pidgeonhole_sort;
+mod cycle_sort;
+mod cockatil_sort;
+mod strand_sort;
+mod bitonic_sort;
+mod pancake_sort;
+mod bogo_sort;
+mod sleep_sort;
+mod stooge_sort;
+mod tree_sort;
+mod brick_sort;
+mod introsort;
+mod tournament_sort;
+mod block_sort;
+mod smoothsort;
+mod patience_sort;
+mod cube_sort;
+mod library_sort;
+mod odd_even_sort;
+mod spread_sort;
+mod burstsort;
+mod flashsort;
+mod postman_sort;
+mod bead_sort;
+mod merge_insertion_sort;
+mod i_cant_believe_it_can_sort_sort;
+mod spaghetti_sort;
+mod network_sort;
+mod bitonic_merge_sort;
+mod slow_sort;
+mod franceschinis_method_sort;
+mod thorups_algorithm_sort;
 
 const LEN: usize = 100;
+const N_ALG:usize=46;
 
-fn main() {
+fn main() { //Considerar  generar threads para cada algoritmo, para intentar que todos se hagan por separado 
     let inicio = Instant::now();
     let arr = generador_array();
     println!("Tiempo generando array: {:?}", inicio.elapsed());
 
-
     let inicio = Instant::now();
     let shuffle_arr = shuffle(arr);
-    println!("{:?}\nTiempo shuffle: {:?}\n", shuffle_arr, inicio.elapsed());
-
+    println!("Shuffle:\n{:?}\nTiempo shuffle: {:?}\nEspacio en memoria del array: {:?}bytes\n", shuffle_arr, inicio.elapsed(),mem::size_of_val(&shuffle_arr));
+    
     let mut aux = shuffle_arr.clone();
     let inicio = Instant::now();
     selection_sort::selection_sort(&mut aux, 0, LEN);
@@ -43,10 +83,11 @@ fn main() {
     assert_eq!(aux, arr);
 
     aux = shuffle_arr.clone();
-
+    let mut arr_trabajo=[0;LEN];
+    
     let inicio = Instant::now(); // Causa un overflow del stack, ya que al utilizar el tipo de dato vec!, que es un tipo dinamico(se utiliza para asignar el tamaño de los vectores de forma dinamica), gasta mas recursos del stack.
-    mergesort::mergesort(&mut aux, 0, LEN);
-    println!("Algoritmo: Merge sort\n{:?}\nTiempo ordenación: {:?}\n", aux, inicio.elapsed());
+    mergesort::top_down_mergesort(&mut aux,&mut arr_trabajo, 0, LEN);
+    println!("Algoritmo: top-down Merge sort\n{:?}\nTiempo ordenación: {:?}\n", aux, inicio.elapsed());
     assert_eq!(aux, arr);
 }
 
--- src/mergesort.rs
+++ src/mergesort.rs
@@ -1,4 +1,5 @@
 // FIXME Este metodo necesita cambios para evitar un overflow del stack
+/*
 pub fn mergesort(arr: &mut [i32], ind1: usize, ind2: usize) {
     if ind1 < ind2 {
         let ind_med = ind1 + (ind2 - ind1) / 2;
@@ -40,5 +41,40 @@ fn merge(arr: &mut [i32], ind1: usize, ind_med: usize, ind3: usize) {
         arr[k] = arr_der[j];
         j = j + 1;
         k = k + 1;
+    }
+}
+*/
+/**
+arr tiene la lista de numeros a ordenar; arr_op es una lista para trabajar
+**/
+//FIXME este código no ordena por alguna razón. debería arreglarlo
+pub fn top_down_mergesort(arr:&mut[i32],arr_op:&mut[i32],ind1:usize,ind2:usize){
+    copiar_array(arr,arr_op,ind1,ind2);
+    top_down_split_merge(arr_op,arr,ind1,ind2);
+}
+fn top_down_merge(arr:&mut[i32],arr_op:&mut[i32],ind1:usize,ind_med:usize,ind2:usize){
+    let mut i=ind1;let mut j=ind_med;
+    for k in ind1..ind2{
+        if i<ind_med &&(j<ind2 || arr[i]<arr[j]) {
+            arr_op[k]=arr[i];
+            i=i+1;
+        }else{
+            arr_op[k]=arr[j];
+            j=j+1;
+        }
+    }
+}
+fn top_down_split_merge(arr:&mut[i32],arr_op:&mut[i32],ind1:usize,ind2:usize){
+    if ind2-ind1<=1 {
+        return;
+    }
+    let ind_med=(ind1+ind2)/2;
+    top_down_split_merge(arr,arr_op,ind1,ind_med);
+    top_down_split_merge(arr,arr_op,ind_med,ind2);
+    top_down_merge(arr,arr_op,ind1,ind_med,ind2);
+}
+fn copiar_array(arr:&mut[i32],arr_op:&mut[i32],ind1:usize,ind2:usize){
+    for i in ind1..ind2{
+        arr_op[i]=arr[i];
     }
 }
